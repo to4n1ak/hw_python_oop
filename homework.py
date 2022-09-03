5420701b2ffc7dedb5a7b62a9c5b98e3399f06ee
Anton Korshunov <042493@mail.ru>
2022-09-03 09:30:55 +0300

    Добавлены комментарии по исправленной версии файла homework.py. 03.09.2022 09:30

--- homework.py
+++ homework.py
@@ -148,7 +148,7 @@ def main(training: Training) -> None:
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
+        ('RN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
 
